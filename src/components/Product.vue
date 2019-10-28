552700d96c5a16bb431c5fce2ea19e6c59935d8d
bhanuteja.a <bhanutejaadapa@gmail.com>
2019-10-28 20:18:00 +0530

    initial commit

---
+++ src/components/Product.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>product list</h1>
+    <div v-for="product in products" :key="product">
+      {{ product.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+import EcommerceServices from '@/services/EcommerceServices.js'
+export default {
+  name: 'products',
+  data() {
+    return {
+      products: []
+    }
+  },
+  mounted() {
+    this.getProducts()
+  },
+  methods: {
+    async getProducts() {
+      const response = await EcommerceServices.fetchProducts()
+      this.products = response.data
+    }
+  }
+}
+</script>
+
+<style></style>
